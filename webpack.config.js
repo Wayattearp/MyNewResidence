91b40d20242e7578f4f00331db94ab8264906636
Wayattearp <iason148348@gmail.com>
2021-10-13 12:01:15 -0400

    Initital setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./frontend/mynewresidence.jsx",
+    output: {
+        path: path.resolve(__dirname, "app", "assets", "javascripts"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/, /\.js?$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/env", "@babel/react"],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    devtool: "eval-source-map",
+    resolve: {
+        extensions: [".js", ".jsx", "*"],
+    },
+};

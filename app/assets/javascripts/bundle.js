f29723c5c49707f8c83eb0559d0ade1a691a2e12
Wayattearp <iason148348@gmail.com>
2021-10-18 21:00:50 -0400

    update scripts

---
+++ app/assets/javascripts/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./frontend/mynewresidence.jsx":
+/*!*************************************!*\
+  !*** ./frontend/mynewresidence.jsx ***!
+  \*************************************/
+/***/ (() => {
+
+eval("// import React from \"react\";\n// import ReactDOM from \"react-dom\";\n// import Root from \"./components/root\";\n// document.addEventListener(\"DOMContentLoaded\", () => {\n//     const root = document.getElementById(\"root\");\n//     ReactDOM.render(<Root />, root);\n// });//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9teW5ld3Jlc2lkZW5jZS8uL2Zyb250ZW5kL215bmV3cmVzaWRlbmNlLmpzeD9mZDUyIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQTtBQUVBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlc0NvbnRlbnQiOlsiLy8gaW1wb3J0IFJlYWN0IGZyb20gXCJyZWFjdFwiO1xuLy8gaW1wb3J0IFJlYWN0RE9NIGZyb20gXCJyZWFjdC1kb21cIjtcbi8vIGltcG9ydCBSb290IGZyb20gXCIuL2NvbXBvbmVudHMvcm9vdFwiO1xuXG4vLyBkb2N1bWVudC5hZGRFdmVudExpc3RlbmVyKFwiRE9NQ29udGVudExvYWRlZFwiLCAoKSA9PiB7XG4vLyAgICAgY29uc3Qgcm9vdCA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKFwicm9vdFwiKTtcbi8vICAgICBSZWFjdERPTS5yZW5kZXIoPFJvb3QgLz4sIHJvb3QpO1xuLy8gfSk7XG4iXSwiZmlsZSI6Ii4vZnJvbnRlbmQvbXluZXdyZXNpZGVuY2UuanN4LmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./frontend/mynewresidence.jsx\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./frontend/mynewresidence.jsx"]();
+/******/ 	
+/******/ })()
+;

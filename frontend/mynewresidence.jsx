0d01ec262dd2405b57a7d5be4fb882bc08d19235
Wayattearp <iason148348@gmail.com>
2021-10-23 09:56:52 -0400

    Set up entry file

--- frontend/mynewresidence.jsx
+++ frontend/mynewresidence.jsx
@@ -1,8 +1,7 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import Root from "./components/root";
+import React from "react";
+import ReactDOM from "react-dom";
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     const root = document.getElementById("root");
-//     ReactDOM.render(<Root />, root);
-// });
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1>Welcome to MyNewResidence</h1>, root);
+});

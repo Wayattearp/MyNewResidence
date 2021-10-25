52df13259069517b2b76febfcd94607454228490
Wayattearp <iason148348@gmail.com>
2021-10-25 13:15:42 -0400

    Set up session and user routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     root to: "static_pages#root"
 
+   namespace :api, defaults: { format: :json } do
+      resources :users, only: [:create, :show, :update]
+      resource :session, only: [:create, :destroy, :show]
+  end
 end

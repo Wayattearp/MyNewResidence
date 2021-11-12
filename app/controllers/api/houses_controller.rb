595fe31c0772b33cd9197c5818718e33d1089494
Wayattearp <iason148348@gmail.com>
2021-11-12 12:32:23 -0500

    Implement controllers

--- app/controllers/api/houses_controller.rb
+++ app/controllers/api/houses_controller.rb
@@ -12,4 +12,9 @@ class Api::HousesController < ApplicationController
         @house = House.create!(house_params)
         render :show
     end
+
+
+  def house_params
+    params.require(:house).permit(:address, :city_id, :state_id, :zipcode, :price, :beds, :baths, :sqft, :is_rent, :lat, :lng, :description, :yr_built, :photo, photos: [])
+  end
 end

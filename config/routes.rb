7e683a5e9326e79b55ffbe3c9bb3ebcdf3e29620
Wayattearp <iason148348@gmail.com>
2021-11-14 20:24:26 -0500

    Add house routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
    namespace :api, defaults: { format: :json } do
       resources :users, only: [:create, :show, :update]
+      resources :houses, only: [:index, :show, :create]
       resources :cities, only: [:index]
       resources :states, only: [:index]
       resource :session, only: [:create, :destroy, :show]

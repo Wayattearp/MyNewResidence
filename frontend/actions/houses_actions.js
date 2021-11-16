9c01a6217f34e9ec2f7772fce6175a647570e0fa
Wayattearp <iason148348@gmail.com>
2021-11-15 20:06:38 -0500

    Add house actions

---
+++ frontend/actions/houses_actions.js
@@ -0,0 +1,35 @@
+import * as HousesAPIUtil from "../util/house_utils";
+
+export const RECEIVE_ALL_HOUSES = "RECEIVE_ALL_HOUSES";
+export const RECEIVE_HOUSE = "RECEIVE_HOUSE";
+
+export const receiveAllHouses = (houses) => {
+    return {
+        type: RECEIVE_ALL_HOUSES,
+        houses,
+    };
+};
+
+export const receiveHouse = (house) => {
+    return {
+        type: RECEIVE_HOUSE,
+        house,
+    };
+};
+
+export const fetchAllHouses = () => {
+    return (dispatch) => {
+        HousesAPIUtil.fetchAllHouses().then((houses) => {
+            return dispatch(receiveAllHouses(houses));
+        });
+    };
+};
+
+
+export const fetchHouse = (houseId) => {
+    return (dispatch) => {
+        HousesAPIUtil.fetchHouse(houseId).then((house) => {
+            return dispatch(receiveHouse(house));
+        });
+    };
+};

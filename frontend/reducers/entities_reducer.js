0554f9032880845209d5e198bd2b6219b440cbc0
Wayattearp <iason148348@gmail.com>
2021-11-15 20:31:30 -0500

    Add houses reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import usersReducer from "./users_reducer";
+import housesReducer from "./houses_reducer";
 
 const entitiesReducer = combineReducers({
-    users: usersReducer
+    users: usersReducer,
+    houses: housesReducer
 });
 
 export default entitiesReducer;

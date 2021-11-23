e74dd7374e86c5547d560fdb7eabe167c5b5c37b
Wayattearp <iason148348@gmail.com>
2021-11-22 19:17:57 -0500

    create houseMap component

---
+++ frontend/components/Houses/house_map.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HouseMap extends React.Component {
+    
+    render() {
+        return (
+            <div id='map-container'
+                 ref='map'>
+
+            </div>
+        );
+    }
+};
+
+export default HouseMap;

faa99462a19a36f74f7d1eab597436326134ca80
Wayattearp <iason148348@gmail.com>
2022-01-18 12:29:56 -0500

    Render google map

--- frontend/components/Houses/house_map.jsx
+++ frontend/components/Houses/house_map.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class HouseMap extends React.Component {
+
+    componentDidMount() {
+        const mapOptions = {
+            center: { lat: 40.80996540451154, lng: -73.95014070092049 }, // this is SF
+            zoom: 15
+        };
+        
+        this.map = new google.maps.Map(this.mapNode, mapOptions);
+    }
     
     render() {
         return (
             <div id='map-container'
-                 ref='map'>
+                ref={map => this.mapNode = map}> // this ref gives us access to the map dom node
 
             </div>
         );

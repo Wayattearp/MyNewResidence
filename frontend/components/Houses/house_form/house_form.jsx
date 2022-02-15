506d3986ccf96275576d9284657f35cb621fdfef
Wayattearp <iason148348@gmail.com>
2022-02-14 20:35:37 -0500

    Add validation alerts

--- frontend/components/Houses/house_form/house_form.jsx
+++ frontend/components/Houses/house_form/house_form.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
+
+
 class HouseForm extends React.Component {
     constructor(props) {
         super(props);
         this.coords = { lat: props.lat, lng: props.lng };
         this.state = {
-            address: '',
-            description: '',
-            state: '',
-            city: '',
-            zipcode: '',
-            price: '',
-            beds: '',
-            baths: '',
-            sqft: '',
-            is_rent: '',
-            yr_built: '',
-
+            address: '48 street',
+            description: 'ui',
+            state: 'NY',
+            city: 'brooklyn',
+            zipcode: 0,
+            price: 0,
+            beds: 0,
+            baths: 0,
+            sqft: 0,
+            is_rent: true,
+            yr_built: 2022,
 
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.navigateToSearch = this.navigateToSearch.bind(this);
+        this.handleInputVals = this.handleInputVals.bind(this);
+        console.log(this.props)
     }
 
     update(property) {
@@ -37,24 +40,61 @@ class HouseForm extends React.Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        const formData = new FormData();
-        formData.append('house[description]', this.state.description);
-        formData.append('house[address]', this.state.address);
-        formData.append('house[state]', this.state.state);
-        formData.append('house[city]', this.state.city);
-        formData.append('house[zipcode]', this.state.zipcode);
-        formData.append('house[price]', this.state.price);
-        formData.append('house[beds]', this.state.beds);
-        formData.append('house[baths]', this.state.baths);
-        formData.append('house[sqft]', this.state.sqft);
-        formData.append('house[is_rent]', this.state.is_rent);
-        formData.append('house[yr_built]', this.state.yr_built);
-        formData.append('house[lat]', this.coords['lat']);
-        formData.append('house[lng]', this.coords['lng']);
+        let isValid = this.handleInputVals();
+        if (isValid) {
+            const formData = new FormData();
+            // formData.append('house[city_id]', this.state.city_id);
+            // formData.append('house[state_id]', this.state.state_id);
+            formData.append('house[description]', this.state.description);
+            formData.append('house[address]', this.state.address);
+            formData.append('house[state]', this.state.state);
+            formData.append('house[city]', this.state.city);
+            formData.append('house[zipcode]', this.state.zipcode);
+            formData.append('house[price]', this.state.price);
+            formData.append('house[beds]', this.state.beds);
+            formData.append('house[baths]', this.state.baths);
+            formData.append('house[sqft]', this.state.sqft);
+            formData.append('house[is_rent]', this.state.is_rent);
+            formData.append('house[yr_built]', this.state.yr_built);
+            formData.append('house[lat]', this.coords['lat']);
+            formData.append('house[lng]', this.coords['lng']);
 
-        this.props.createHouse(formData);
-        this.navigateToSearch();
+            this.props.createHouse(formData)
+        }
     }
+
+    handleInputVals() {
+        if (!isNaN(parseInt(this.state.city))) {
+            window.alert("City name should be String");
+            return false;
+        }
+        if (!isNaN(parseInt(this.state.state))) {
+            window.alert("State name should be String");
+            return false;
+        }
+        if (isNaN(parseInt(this.state.zipcode)) || this.state.zipcode.length != 5) {
+            window.alert("Zipcode value should be 5 digits Integer");
+            return false;
+        }
+        if (isNaN(parseInt(this.state.price))) {
+            window.alert("Price value should be Integer");
+            return false;
+        }
+        if (isNaN(parseInt(this.state.baths))) {
+            window.alert("Bathrooms value should be Integer");
+            return false;
+        }
+        if (isNaN(parseInt(this.state.beds))) {
+            window.alert("Bedrooms value should be Integer");
+            return false;
+        }
+        if (isNaN(parseInt(this.state.sqft))) {
+            window.alert("Sqft value should be Integer");
+            return false;
+        }
+
+        return true;
+    };
 
     render() {
         const { lat, lng } = this.coords;
@@ -81,12 +121,12 @@ class HouseForm extends React.Component {
                             className='house-field'
                         />
                         <label className='new-house-state'> State </label>
-                        <input
-                            type="text"
-                            value={state}
+                        <select
                             onChange={this.update('state')}
-                            className='house-field'
-                        />
+                            className='house-field'>
+                            <option value="NY">NY</option>
+                            <option value="NJ">NJ</option>
+                        </select>
                         <label className='new-house-city'> City </label>
                         <input
                             type="text"
@@ -96,7 +136,7 @@ class HouseForm extends React.Component {
                         />
                         <label className='new-house-zipcode'> Zipcode </label>
                         <input
-                            type="text"
+                            type="number"
                             value={zipcode}
                             onChange={this.update('zipcode')}
                             className='house-field'
@@ -124,18 +164,18 @@ class HouseForm extends React.Component {
                         />
                         <label className='new-house-sqft'> Sqft </label>
                         <input
-                            type="text"
+                            type="number"
                             value={sqft}
                             onChange={this.update('sqft')}
                             className='house-field'
                         />
                         <label className='new-house-is_rent'> Currently available for rent </label>
-                        <input
-                            type="text"
-                            value={is_rent}
+                        <select
                             onChange={this.update('is_rent')}
-                            className='house-field'
-                        />
+                            className='house-field'>
+                            <option value="NY">Available</option>
+                            <option value="NJ">N/A</option>
+                        </select>
                         <label className='new-house-yr_built'> Year built </label>
                         <input
                             type="text"
@@ -144,17 +184,17 @@ class HouseForm extends React.Component {
                             className='house-field'
                         />
 
-                        <label className='house-field'>Latitude</label>
+                        <label className='house-lat'>Latitude</label>
                         <input
-                            type="text"
+                            type="number"
                             disabled
                             value={lat}
                             className='house-field'
                         />
 
-                        <label className='house-field'>Longitude</label>
+                        <label className='house-lng'>Longitude</label>
                         <input
-                            type="text"
+                            type="number"
                             disabled
                             value={lng}
                             className='house-field'

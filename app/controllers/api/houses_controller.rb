c2d5aff594218163f72160d30e90ef5ef6ed58a5
Wayattearp <iason148348@gmail.com>
2022-02-16 21:09:07 -0500

    Add house create through ui

--- app/controllers/api/houses_controller.rb
+++ app/controllers/api/houses_controller.rb
@@ -11,8 +11,32 @@ class Api::HousesController < ApplicationController
     end
     
     def create
-        @house = House.create!(house_params)
-        render :show
+        city = "%#{params[:house][:city].strip.split(//).join("%")}%"
+        state = "%#{params[:house][:state].strip.split(//).join("%")}%"
+        city_id = City.where("UPPER(TRIM(name)) LIKE UPPER(?)", city).pluck(:id)[0]
+        state_id = State.where("UPPER(TRIM(name)) LIKE UPPER(?)", state).pluck(:id)[0]
+
+      if (!city_id)
+        new_city = City.create!(name: city.split("%").join("").capitalize)
+        city_id = new_city.id
+       end
+
+      if (!state_id)
+        new_state = State.create!(name: state.split("%").join("").upcase)
+        state_id = new_state.id
+      end
+
+        params[:house][:city_id] = city_id
+        params[:house][:state_id] = state_id
+        @house = House.new(house_params)
+        # @house.city = City.find_by(id: city_id)
+        # @house.state = State.find_by(id: state_id)
+
+        if @house.save
+          render :show
+        else
+          render json: @house.errors.full_messages, status: 422
+        end
     end
 
     

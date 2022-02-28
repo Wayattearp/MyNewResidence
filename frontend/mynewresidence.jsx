03ce98c1b23fdbe83304de083cdebb16c421d74b
Wayattearp <iason148348@gmail.com>
2022-02-27 20:29:59 -0500

    Clean up tests

--- frontend/mynewresidence.jsx
+++ frontend/mynewresidence.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
-// import { fetchAllHouses } from './actions/houses_actions'
 
 document.addEventListener("DOMContentLoaded", () => {
     let store;
@@ -22,10 +21,5 @@ document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     ReactDOM.render(<Root store={store} />, root);
 
-    //TESTS 
-    // window.getState = store.getState;
-    // window.dispatch = store.dispatch;
-
-    // window.fetchAllHouses = fetchAllHouses;
 });
 

0663387c58aae634059024dee5570c110bf0db1f
Wayattearp <iason148348@gmail.com>
2022-03-21 18:01:41 -0400

    Render house index list

--- frontend/components/Houses/house_index.jsx
+++ frontend/components/Houses/house_index.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import HouseIndexItem from './house_index_item';
 
- class HouseIndex extends React.Component {
-     constructor(props){
-         super(props)
-         
-     }
-    componentDidMount() {
-        
-        }
-
-        render() {
-            
-            let houses = this.props.houses;
-
-
-        return(
-                <div>
-               
-                </div>
-            );
-     }
-    };
+const HouseIndex = ({ houses }) => (
+    <div>
+        <h1>Houses: </h1>
+        {houses.map(house => (
+            <HouseIndexItem
+                house={house}
+                key={house.id}
+            />
+        ))}
+    </div>
+);
 
 export default HouseIndex;

d09ccea45a466fd7c41a66c5cecb7d3582e715af
Wayattearp <iason148348@gmail.com>
2022-06-01 19:52:44 -0400

    Add filter actions

--- frontend/actions/filter_actions.js
+++ frontend/actions/filter_actions.js
@@ -1,14 +1,47 @@
 import { fetchAllHouses } from "./houses_actions";
 
 export const UPDATE_FILTER = 'UPDATE_FILTER';
+export const CLEAR_ALL_FILTER = "CLEAR_ALL_FILTER";
+export const CLEAR_FILTER = "CLEAR_FILTER";
 
-export const changeFilter = (filter, value) => ({
-    type: UPDATE_FILTER,
-    filter,
-    value
-});
+
+export const changeFilter = (filter, value) => {
+    // debugger
+    return {
+        type: UPDATE_FILTER,
+        filter,
+        value,
+    };
+};
+
+export const clearFilter = (filter) => {
+    return {
+        type: CLEAR_FILTER,
+        filter,
+    };
+};
+
+export const clearAllFilters = () => {
+    return {
+        type: CLEAR_ALL_FILTER,
+    };
+};
 
 export const updateFilter = (filter, value) => (dispatch, getState) => {
     dispatch(changeFilter(filter, value));
     return fetchAllHouses(getState().ui.filters)(dispatch);
 };
+
+export function removeFilter(filter) {
+    return (dispatch, getState) => {
+        dispatch(clearFilter(filter));
+        return fetchAllHouses(getState().ui.filters)(dispatch);
+    };
+}
+
+export function removeAllFilters() {
+    return (dispatch, getState) => {
+        dispatch(clearAllFilters());
+        return fetchAllHouses(getState().ui.filters)(dispatch);
+    };
+}

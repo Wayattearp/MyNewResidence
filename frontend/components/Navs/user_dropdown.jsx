9682b03e9d1f0abf032e8326e95d0400bf273dee
Wayattearp <iason148348@gmail.com>
2022-06-01 19:49:52 -0400

    Add user drop down menue

---
+++ frontend/components/Navs/user_dropdown.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { logout } from "../../actions/session_actions";
+
+const UserDropDown = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dispatch = useDispatch();
+
+  const openDropDown = (e) => {
+    setIsOpen(true);
+  };
+
+  const closeDropDown = (e) => {
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 100);
+  };
+  return (
+    <button
+      onFocus={openDropDown}
+      onBlur={closeDropDown}
+      className="fas fa-user-circle">
+      <div id="user-dropdown" className={isOpen ? "show" : "hidden"}>
+        <a onClick={() => dispatch(logout())}> Sign out</a>
+      </div>
+    </button>
+  );
+};
+
+export default UserDropDown;

524d266075f4e0e2ae3f058696091ab784e185c9
Wayattearp <iason148348@gmail.com>
2022-06-01 19:55:47 -0400

    Add no listings msg

--- frontend/components/Houses/house_index.jsx
+++ frontend/components/Houses/house_index.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import HouseIndexItem from './house_index_item';
 
-const HouseIndex = ({ houses }) => (
-    <div className="property-index-container">
-        {houses.map(house => (
+const HouseIndex = ({ houses }) => {
+
+    let filteredHouses;
+
+    const mappedHouses = houses.map((house, i) => {
+        return (
             <HouseIndexItem
+                key={`house-${i}`}
                 house={house}
-                key={house.id}
             />
-        ))}
-    </div>
-);
+        );
+    });
+    return (
+        <div className="property-index-container">
+            {mappedHouses.length > 0 ? (
+                mappedHouses
+            ) : (
+                <h1 className="no-listing"> "No Listing available.." </h1>
+            )}
+        </div>
+    )
+};
 
 export default HouseIndex;

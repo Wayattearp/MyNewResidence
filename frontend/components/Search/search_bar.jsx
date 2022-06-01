48a4053f9b21a4927ffd8ad0e0022af8c9943c9d
Wayattearp <iason148348@gmail.com>
2022-06-01 19:53:33 -0400

    Add search bar

---
+++ frontend/components/Search/search_bar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router";
+import { updateFilter } from "../../actions/filter_actions";
+
+const SearchBar = (props) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const [query, setQuery] = useState("");
+
+  const handleAreaSearch = (e) => {
+    // debugger
+    //setQuery
+    console.log(e.currentTarget)
+    console.log(e.currentTarget.value)
+    setQuery(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //update filter with state
+    dispatch(updateFilter("query", query));
+    history.push("/search")
+  };
+
+  return (
+    <form className="search-bar-form" onSubmit={handleSubmit}>
+      <input
+        id="search-bar"
+        type="search"
+        value={query}
+        placeholder="Enter a City, State, or ZIP code"
+        onChange={handleAreaSearch}
+      />
+      <button className="search-btn">
+        <i className="fas fa-search"></i>
+      </button>
+    </form>
+  );
+};
+
+export default SearchBar;

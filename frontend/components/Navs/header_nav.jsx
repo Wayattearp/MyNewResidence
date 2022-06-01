6c8e1c44adaf3e515644154501800191bf029407
Wayattearp <iason148348@gmail.com>
2022-06-01 19:55:17 -0400

    Add logo to the header, style

--- frontend/components/Navs/header_nav.jsx
+++ frontend/components/Navs/header_nav.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import SignInNav from "./sign_in_nav_container"
+import UserIcon from "./user_icon";
+import Modal from "../Modal/modal"
+
 const HeaderNav = () => {
-      return (
-        <div className="NavHeader">
-            <div className="NavHeader-left">
-                <Link to="/search"> Buy </Link>
-                <Link to="/search"> Rent </Link>
-                <Link to="/sell"> Sell </Link>
-            </div>
-              <div className="NavHeader-right">
-                <SignInNav />
-            </div>
-        </div>
+  return (
+    <div className="NavHeader">
+      <div className="NavHeader-left">
+        <Link to="/search"> Buy </Link>
+        <Link to="/search"> Rent </Link>
+        <Link to="/sell"> Sell </Link>
+      </div>
+      <Link to="/" className="logo-container">
+        <img className="logo" src={window.logoURL} />
+      </Link>
+      <div className="NavHeader-right">
+        <a> LinkedIn</a>
+        <a href="https://github.com/Wayattearp">Github</a>
+        <UserIcon />
+        <Modal />
+      </div>
+    </div>
   );
 };
 
